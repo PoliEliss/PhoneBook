ed957f20907a933abeea3f80e01ed5911466e436
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-03 18:51:25 +0300

    add date with telephoneBook in HasMap

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,19 +3,12 @@ import java.io.File
 fun main(args: Array<String>) {
 
 
-
-
     val input = File("TelepfoneBook") // это у меня получился неизменяемый List<String>
         .readLines()
-        //.withIndex()
-       // .map { indexedValue -> indexedValue.index to indexedValue.value }
-        //.toMap()
-    println(input.get(2))
 
-    val listPhone = input.filter { it.startsWith("8") }
-    val listName = input.filter { it.length <10 }
-    println(listName) // у нас есть список номеров
-    println(listPhone) // и есть список имен 
+    val listPhone = input.filter { it.startsWith("8") }// у нас есть список номеров
+    val listName = input.filter { it.length < 10 }// и есть список имен
+
 
     val contactIvan = Contacts("89675467", "Ivan")
     val contactOlga = Contacts("89517569876", "Olga")
@@ -26,11 +19,16 @@ fun main(args: Array<String>) {
     contactsTel.put(contactIvan.phoneNumber, contactIvan.name)
     contactsTel.put(contactBoris.phoneNumber, contactBoris.name)
 
-    for (key in contactsTel.keys){
-      //  println("$key = ${contactsTel[key]}")
+    var count = 0
+    while (count < listName.size && count < listPhone.size) {
+        contactsTel.put(listPhone[count], listName[count])
+        count++
     }
+    println(contactsTel)
 
-
+    for (key in contactsTel.keys) {
+        println("$key = ${contactsTel[key]}")
+    }
 }
 
 
