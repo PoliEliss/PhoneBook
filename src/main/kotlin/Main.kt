82b9bb40d711421c25190cf166709d6c9555bdad
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-04-03 21:43:10 +0300

    change HasMap

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,10 @@ import java.io.File
 fun main(args: Array<String>) {
 
 
+
+
+
+
     val input = File("TelepfoneBook") // это у меня получился неизменяемый List<String>
         .readLines()
 
@@ -10,13 +14,12 @@ fun main(args: Array<String>) {
     val listName = input.filter { it.length < 10 }// и есть список имен
 
 
-    val contactIvan = Contacts("89675467", "Ivan")
-    val contactOlga = Contacts("89517569876", "Olga")
-    val contactBoris = Contacts("896754239", "Boris")
 
-    val contactsTel = HashMap<String, String>() // я пока представлю номер телефона типом String
+    val contactOlga = Contacts("89517569876", "Анна")
+    val contactBoris = Contacts("896754239", "Ева")
+
+    val contactsTel = hashMapOf<String,String>() // я пока представлю номер телефона типом String
     contactsTel.put(contactOlga.phoneNumber, contactOlga.name)
-    contactsTel.put(contactIvan.phoneNumber, contactIvan.name)
     contactsTel.put(contactBoris.phoneNumber, contactBoris.name)
 
     var count = 0
